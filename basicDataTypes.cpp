0782e03753cf30ffd443ba8e64ccea72c78c2a5e
Prerna <prerna1998.ps@gmail.com>
2020-03-02 02:53:26 +0530

    Basic Data Types

---
+++ basicDataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    int a;long b;char c;float d;double e;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cin>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<fixed<<setprecision(3)<<d<<endl;
+    cout<<fixed<<setprecision(9)<<e<<endl;
+    return 0;
+}
